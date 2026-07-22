f008f3aa13612db6e259377bf5e92909cbcc7b27
Josh McDonald <joshymcd92@gmail.com>
2026-07-23 00:20:52 +0100

    open organization setup from home

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,18 +1,6 @@
 import { Navigate, createFileRoute } from '@tanstack/react-router'
-import { useLiveQuery } from '@tanstack/react-db'
-import { organizationsCollection } from '../collections/organization'
 
 export const Route = createFileRoute('/')({ component: Home })
 function Home() {
-  const organizations = useLiveQuery(() => organizationsCollection).data ?? []
-  const { organizationId } = Route.useSearch()
-  const organization =
-    organizations.find((item) => item.id === organizationId) ??
-    organizations.at(0)
-  return (
-    <Navigate
-      to={organization ? '/overview' : '/setup'}
-      search={organization ? { organizationId: organization.id } : {}}
-    />
-  )
+  return <Navigate to="/setup" />
 }
