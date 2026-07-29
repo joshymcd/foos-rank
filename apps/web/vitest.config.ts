33ea4ce4155daa7cca77e245614445c598d97c8a
Josh McDonald <joshymcd92@gmail.com>
2026-07-29 08:56:17 +0100

    Remove server configuration from vitest config

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -4,9 +4,5 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['src/**/*.test.ts'],
-  },
-  server: {
-      preset: "aws-lambda",
-      awsLambda: { streaming: false }, 
-    }
+  }
 })
